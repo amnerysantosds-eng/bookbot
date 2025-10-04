284dea14d033d9a017f74de00749b5166372488f
amnerysantosds-eng <amnerysantosds@gmail.com>
2025-10-04 11:09:09 -0400

    added funct for word count and book content

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boot")
+def get_book_test(filepath):
+    with open(filepath) as file:
+        return file.read()
+    
+def get_word_count(book_content):
+    return book_content.split()
+
+def main():
+    filepath = "books/frankenstein.txt"
+    book_content = get_book_test(filepath)
+    num_words = get_word_count(book_content)
+
+main()
