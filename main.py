ab59a22f1e9f5d11e246d5ef2aa3185959ab71a5
amnerysantosds-eng <amnerysantosds@gmail.com>
2025-10-07 17:34:03 -0400

    added functionaly to print report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_character_count
+import stats
 
 def get_book_test(filepath):
     with open(filepath) as file:
@@ -7,7 +7,17 @@ def get_book_test(filepath):
 def main():
     filepath = "books/frankenstein.txt"
     book_content = get_book_test(filepath)
-    character_dictionary = get_character_count(book_content)
-    print(character_dictionary)
-
+    word_count = stats.get_word_count(book_content)
+    character_dictionary = stats.get_character_count(book_content)
+    lst_dictionaries = stats.get_lst_dictionaries(character_dictionary)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    
+    for dictionary in lst_dictionaries:
+        if dictionary["char"].isalpha():
+            print(f"{dictionary["char"]}: {dictionary["num"]}")  
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
 def get_word_count(book_content):
-    return book_content.split()
+    return len(book_content.split())
 
 def get_character_count(book_content):
     book_content_cleaned = book_content.lower()
@@ -11,3 +11,17 @@ def get_character_count(book_content):
         else:
             character_dictionary[character] = 1
     return character_dictionary
+
+def sort_on(dictionary):
+    return dictionary["num"]
+
+def get_lst_dictionaries(character_dictionary):
+    lst_dictionaries = list()
+    
+    for key, value in character_dictionary.items():
+        lst_dictionaries.append({"char": key, "num":value})
+    lst_dictionaries.sort(key=sort_on, reverse=True)
+
+    return lst_dictionaries
+    
+    
